e38fd69e0a0b71dd93655aa15ca6482a62eed2a4
Flavio Oliveira <flavio.m@samsung.com>
2019-01-11 07:59:10 -0200

    Creating the sample app

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class SearchBar extends React.Component { 
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text">
+          </input>
+        </form>
+      </div>
+    );
+  }
+}
+
+
+export default SearchBar;
